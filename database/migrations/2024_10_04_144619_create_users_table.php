0bef526eb0be303a7e91407dde3e764986017903
AminovJamshid <aminovjamshid777@gmail.com>
2024-10-14 15:31:48 +0500

    PATCH: added mini changes

--- database/migrations/2024_10_04_144619_create_users_table.php
+++ database/migrations/2024_10_04_144619_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('position');
+            $table->string('name');
+            $table->string('position')->nullable();
             $table->enum("gender", ["male", "female"]);
             $table->string('email')->unique();
             $table->string("phone");
